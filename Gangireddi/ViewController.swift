2d658c894bd14d6d29c384c82bf17db716f606aa
sandeepgangireddi <s.gangireddi@pccsuk.com>
2022-01-31 10:42:52 +0530

    initial commit

--- Gangireddi/ViewController.swift
+++ Gangireddi/ViewController.swift
@@ -9,8 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let urls = [
+        URL(string: "https://github.com/fluffyes/AppStoreCard/archive/master.zip")!,
+        URL(string: "https://github.com/fluffyes/currentLocation/archive/master.zip")!,
+        URL(string: "https://github.com/fluffyes/DispatchQueue/archive/master.zip")!,
+        URL(string: "https://github.com/fluffyes/dynamicFont/archive/master.zip")!,
+        URL(string: "https://github.com/fluffyes/telegrammy/archive/master.zip")!
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
+        
+        
+
+        // call multiple urlsession downloadtask, these will all run at the same time!
+        for url in urls {
+            print("start fetching \(url.absoluteString)")
+            URLSession.shared.downloadTask(with: url, completionHandler: { (tempURL, response, error) in
+                print("finished fetching \(url.absoluteString)")
+            }).resume()
+        }
+        
         // Do any additional setup after loading the view.
     }
 
